7fb25c5846da3c1dede52e95c8ed450b403e8ab4
julien-ctx <jdaodkql@gmail.com>
2022-12-13 11:11:52 +0100

    ClimbingStairs

---
+++ easy/ClimbingStairs.hpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        if (n < 4)
+            return n;
+        int prev1 = 2; int prev2 = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            int tmp = prev1;
+            prev1 += prev2;
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+};
+
+// Recursive alternative but timeout:
+
+// class Solution {
+// public:
+//     int climbStairs(int n)
+//     {
+//         if (n < 4)
+//             return n;
+//         else
+//             return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
