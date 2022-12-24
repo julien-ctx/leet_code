891ca21caee358ba2f4b8c54fee481f0113fe8d6
Julien CAUCHETEUX <jdaodkql@gmail.com>
2022-12-24 17:31:59 +0100

    GenerateParentheses backtracking ok

--- medium/GenerateParentheses.hpp
+++ medium/GenerateParentheses.hpp
@@ -8,27 +8,20 @@ public:
     vector<string> generateParenthesis(int n)
     {
         vector<string> cbn;
-        
-        backtracking(cbn, n, 0, 0, "");
-        return cbn;
+        return backtracking(cbn, n, 0, 0, "");
     }
 
-    void backtracking(vector<string> &cbn, int &n, int opened, int closed, string s)
+    vector<string> backtracking(vector<string> &cbn, int n, int opened, int closed, string s)
     {
         if (opened + closed == n * 2)
         {
             cbn.push_back(s);
-            return;
+            return cbn;
         }
-        else if (opened < n)
-        {
-            s += '(';
-            backtracking(cbn, n, opened + 1, closed, s);
-        }
-        else if (closed < n)
-        {
-            s += ')';
-            backtracking(cbn, n, opened, closed + 1, s);
-		}
+        if (opened < n )
+            backtracking(cbn, n, opened + 1, closed, s + '(');
+        if (closed < opened)
+            backtracking(cbn, n, opened, closed + 1, s + ')');
+        return cbn;
     }
 };
